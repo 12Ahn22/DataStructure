78d6295543e8ee4199eef3de6a88e6bf7f4e9ff1
Ayo <12ayo22@gmail.com>
2022-11-27 16:19:08 +0900

    [11/27] 그래프 순회

--- Graph/AdjacencyListGraph.js
+++ Graph/AdjacencyListGraph.js
@@ -1,6 +1,16 @@
 class AdjacencyListGraph {
   constructor() {
-    this.adjacencyList = {};
+    // this.adjacencyList = {};
+
+    // 테스트용 그래프 데이터
+    this.adjacencyList = {
+      A: ["B", "C"],
+      B: ["A", "D"],
+      C: ["A", "E"],
+      D: ["B", "E", "F"],
+      E: ["C", "D", "F"],
+      F: ["D", "E"],
+    };
   }
 
   // add vertext
@@ -52,13 +62,84 @@ class AdjacencyListGraph {
 
     delete this.adjacencyList[vertex];
   }
+
+  // 순회
+  DFSrecursive(start) {
+    // 결과를 저장하는 배열
+    const result = []; // 순서
+    const visited = {}; // 방문 vertex 배열
+    const adjacencyList = this.adjacencyList; // 즉시 실행 함수 내부에서 this는 전역 객체기 때문에 변수로 선언해서 전달함
+
+    // 즉시 실행 함수
+    (function dfs(vertex) {
+      // Base Case
+      if (!vertex) return null;
+
+      // 방문 처리
+      visited[vertex] = true;
+      result.push(vertex);
+      adjacencyList[vertex].forEach((neighbor) => {
+        // 방문하지 않았다면 재귀 시킨다.
+        if (!visited[neighbor]) {
+          return dfs(neighbor);
+        }
+      });
+    })(start);
+
+    return result;
+  }
+
+  // 반복문 사용
+  DFSIterative(start) {
+    const stack = [start];
+    const result = [];
+    const visited = {};
+
+    visited[start] = true;
+    let currentVertext;
+    // stack에 값이 있다면 계속 확인하기
+    while (stack.length) {
+      currentVertext = stack.pop();
+      result.push(currentVertext);
+
+      this.adjacencyList[currentVertext].forEach((neighbor) => {
+        if (!visited[neighbor]) {
+          visited[neighbor] = true;
+          stack.push(neighbor);
+        }
+      });
+    }
+    return result;
+  }
+
+  // BFS 너비 우선 탐색
+  BFSIterative(start) {
+    const result = [];
+    const queue = [start];
+    const visited = {};
+    visited[start] = true;
+
+    let currentVertex;
+    while (queue.length) {
+      currentVertex = queue.shift();
+      result.push(currentVertex);
+
+      this.adjacencyList[currentVertex].forEach((neighbor) => {
+        if (!visited[neighbor]) {
+          visited[neighbor] = true;
+          queue.push(neighbor);
+        }
+      });
+    }
+    return result;
+  }
 }
 
 const g = new AdjacencyListGraph();
-g.addVertex("A");
-g.addVertex("B");
-g.addVertex("C");
+// g.addVertex("A");
+// g.addVertex("B");
+// g.addVertex("C");
 
-g.addEdge("A", "B");
-g.addEdge("A", "C");
+// g.addEdge("A", "B");
+// g.addEdge("A", "C");
 // g.removeEdge("A", "B");
